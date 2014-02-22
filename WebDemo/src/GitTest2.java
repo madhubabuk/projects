4a034bc8cd5284337bacb02ab8eb41ba03466c6b
madhu <babu7uc@yahoo.com>
2014-02-22 12:55:19 +0530

    32

--- WebDemo/src/GitTest2.java
+++ WebDemo/src/GitTest2.java
@@ -3,7 +3,7 @@ public class GitTest2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("GitTest2");
+		System.out.println("GitTest3");
 	}
 
 }
