400f9be6e223a228299d4bec5cf2b940936ae5e1
madhu <babu7uc@yahoo.com>
2014-02-23 11:43:56 +0530

    today

--- WebDemo/src/GitTest.java
+++ WebDemo/src/GitTest.java
@@ -1,10 +1,18 @@
 
+/**
+ * @author Lalli
+ *
+ */
 public class GitTest {
 
+	/**
+	 * This is gittest documentation
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Git is fun"); 
-		System.out.println("Git is fun2");
+		System.out.println("Git is fun2"); 
 	}
 
 }
