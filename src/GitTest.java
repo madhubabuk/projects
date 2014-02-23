5ddd60568df062bd9a7e11e91b24889c013b11a4
madhu <babu7uc@yahoo.com>
2014-02-23 12:19:11 +0530

    test

---
+++ src/GitTest.java
@@ -0,0 +1,18 @@
+
+/**
+ * @author Lalli
+ *
+ */
+public class GitTest {
+
+	/**
+	 * This is gittest documentation
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Git is fun"); 
+		System.out.println("Git is fun2"); 
+	}
+
+}
